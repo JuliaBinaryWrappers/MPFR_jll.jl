db308b1b02500661ca0f53a94b4f4fc45ff0cc64
jlbuild <juliabuildbot@gmail.com>
2020-10-18 20:32:45 +0000

    MPFR_jll build 4.0.2+3

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -2,52 +2,15 @@
 export libmpfr
 
 using GMP_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `libmpfr`
-const libmpfr_splitpath = ["bin", "libmpfr-6.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libmpfr_path = ""
-
-# libmpfr-specific global declaration
-# This will be filled out by __init__()
-libmpfr_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libmpfr = "libmpfr-6.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("MPFR")
+JLLWrappers.@declare_library_product(libmpfr, "libmpfr-6.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"MPFR")
+    JLLWrappers.@generate_init_header(GMP_jll)
+    JLLWrappers.@init_library_product(
+        libmpfr,
+        "bin\\libmpfr-6.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (GMP_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (GMP_jll.LIBPATH_list,))
-
-    global libmpfr_path = normpath(joinpath(artifact_dir, libmpfr_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libmpfr_handle = dlopen(libmpfr_path)
-    push!(LIBPATH_list, dirname(libmpfr_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
